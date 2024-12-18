6d9428560289b89f65b2d8e645e5a5f065dca312
Edward <dortaedward@gmail.com>
2024-12-17 21:07:14 -0500

    inital commit

---
+++ src/types/generic.types.ts
@@ -0,0 +1,19 @@
+
+// docs
+
+type User = {}
+
+
+type CardState = "new" | "learing" | "review_young" | "review_mature" | "relearn";
+
+// Cards can be in multiple deck
+type Card = { 
+  question: string;
+  answer: string;
+  images: string[]?;
+  state: CardState;
+}
+
+type Deck = { }
+
+type Category = { }
